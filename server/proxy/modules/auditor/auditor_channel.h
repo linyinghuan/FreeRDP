7eceb4b3a069a5cdfbf7cf20d614eb31db735670
linying.huan <35327717@qq.com>
2024-08-30 21:44:54 -0400

    support session context

--- server/proxy/modules/auditor/auditor_channel.h
+++ server/proxy/modules/auditor/auditor_channel.h
@@ -12,10 +12,10 @@ typedef enum{
 	AUDITOR_EVENT_MAX,
 } AUDITOR_EVENT_TYPE;
 
-typedef VOID(*AUDITOR_EVENT_CB)(proxyData* pdata, proxyChannelDataEventInfo* pEvent);
+typedef VOID(*AUDITOR_EVENT_CB)(proxyData* pdata, proxyChannelDataEventInfo* pEvent, AUDITOR_CTX_DATA *auditor_ctx);
 
 BOOL auditor_server_channels_init(proxyData* pdata);
-BOOL auditor_client_channel_handler(proxyData* data, void* context, AUDITOR_CTX_DATA *auditor_ctx);
-BOOL auditor_server_channel_handler(proxyData* data, void* context, AUDITOR_CTX_DATA *auditor_ctx);
+BOOL auditor_client_channel_handler(proxyData* data, void* context);
+BOOL auditor_server_channel_handler(proxyData* data, void* context);
 BOOL auditor_channel_event_reg(UINT32 mode, UINT32 type, AUDITOR_EVENT_CB callback);
 #endif
